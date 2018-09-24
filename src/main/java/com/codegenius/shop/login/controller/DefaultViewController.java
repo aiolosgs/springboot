f393adc12b064ac336563a05fa9a0a81b9146167
huangsiyu <aiolosgs@163.com>
2018-09-24 16:57:49 +0800

    自定义配置

--- src/main/java/com/codegenius/shop/login/controller/DefaultViewController.java
+++ src/main/java/com/codegenius/shop/login/controller/DefaultViewController.java
@@ -1,11 +1,21 @@
 package com.codegenius.shop.login.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DefaultViewController{
 
+	@Value("${test.config}")
+	private String config;
+	
+	@Value("${security.config}")
+	private String security;
+	
+	@Value("${system.config}")
+	private String system;
+	
 	@RequestMapping("/")
     public String index() {
 		System.out.println("go to index");
@@ -14,6 +24,9 @@ public class DefaultViewController{
 	
 	@RequestMapping("login")
 	public String login(){
+		System.out.println(security);
+		System.out.println(system);
+		System.out.println(config);
 		System.out.println("go to login");
 		return "login";
 	}
